1315b9255c1d19a16bf2b2347877a443f9b173d0
babarobat <36125153+babarobat@users.noreply.github.com>
2018-11-14 23:28:46 +0300

    Walk Animation

--- Bomber/Assets/Scripts/PlayerController.cs
+++ Bomber/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
-
+[RequireComponent (typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
 public class PlayerController : MonoBehaviour
 {
 
@@ -10,15 +11,39 @@ public class PlayerController : MonoBehaviour
     private Tilemap _tilemap;
     [SerializeField]
     private Bomb _bombPrefab;
-	void Update () {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Vector3 worldPos =  Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector3Int cell = _tilemap.WorldToCell(worldPos);
-            Vector3 cellCenterPos = _tilemap.GetCellCenterWorld(cell);
+    [SerializeField]
+    private Transform _bombSpawnPoint;
+    [SerializeField]
+    private float _speed;
+    
+    private Animator _animator;
 
+    private Rigidbody2D _rb;
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+       
+        _animator = GetComponent<Animator>();
+    }
+    void Update ()
+    {
+        var hor = Input.GetAxisRaw("Horizontal");
+        var vert = Input.GetAxisRaw("Vertical");
+        _animator.SetFloat("VelocityX", hor);
+        _animator.SetFloat("VelocityY", vert);
+        if (Input.GetButtonDown("Jump"))
+        {
+            
+            Vector3Int cell = _tilemap.WorldToCell(_bombSpawnPoint.position);
+            Vector3 cellCenterPos = _tilemap.GetCellCenterWorld(cell);
             Instantiate(_bombPrefab, cellCenterPos, Quaternion.identity);
-             
         }
+        if (hor != 0 || vert !=0)
+        {
+            
+            _rb.MovePosition( new Vector3(transform.position.x + hor* _speed * Time.deltaTime, transform.position.y+vert * _speed * Time.deltaTime,0));
+        }
+        
 	}
+
 }
